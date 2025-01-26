5fbc2f0dcd79bbf06c92b322b15450897eb3d18f
MrReedQEGS <60434301+MrReedQEGS@users.noreply.github.com>
2025-01-26 12:48:16 +0000

    Update EnigmaMain.py
    Swapping to numerical mappings for the rotors

--- EnigmaMain.py
+++ EnigmaMain.py
@@ -27,33 +27,53 @@
 #Tech details about ring settings, etc.
 # https://www.ciphermachinesandcryptology.com/en/enigmatech.htm
 
-
-ETW	        = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-ROTOR_I     = ["E","K","M","F","L","G","D","Q","V","Z","N","T","O","W","Y","H","X","U","S","P","A","I","B","R","C","J"]
-ROTOR_II    = ["A","J","D","K","S","I","R","U","X","B","L","H","W","T","M","C","Q","G","Z","N","P","Y","F","V","O","E"]
-ROTOR_III   = ["B","D","F","H","J","L","C","P","R","T","X","V","Z","N","Y","E","I","W","G","A","K","M","U","S","Q","O"]
-ROTOR_IV    = ["E","S","O","V","P","Z","J","A","Y","Q","U","I","R","H","X","L","N","F","T","G","K","D","C","M","W","B"]
-ROTOR_V     = ["V","Z","B","R","G","I","T","Y","U","P","S","D","N","H","L","X","A","W","M","J","Q","O","F","E","C","K"]
-UKW_A       = ["E","J","M","Z","A","L","Y","X","V","B","W","F","C","R","Q","U","O","N","T","S","P","I","K","H","G","D"]	 	 	 
-UKW_B       = ["Y","R","U","H","Q","S","L","D","P","X","N","G","O","K","M","I","E","B","F","Z","C","W","V","J","A","T"]	 	 	 
-UKW_C       = ["F","V","P","J","I","A","O","Y","E","D","R","Z","X","W","G","C","T","K","U","Q","S","B","N","M","H","L"]
-
 def LetterToPos(someLetter):
     val = ord(someLetter)
     return val - 65
 
+def PosToLetter(somePos):
+    letter = chr(somePos + 65)
+    return letter
+
+def TurnRotorsLettersIntoMappingNumbers(someList):
+    answer = []
+    for letter in someList:
+        answer.append(LetterToPos(letter))
+    return answer
+
+ETW	        = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+ROTOR_I     = "EKMFLGDQVZNTOWYHXUSPAIBRCJ"
+ROTOR_I_NUMS = TurnRotorsLettersIntoMappingNumbers(ROTOR_I)
+
+ROTOR_II    = "AJDKSIRUXBLHWTMCQGZNPYFVOE"
+ROTOR_II_NUMS = TurnRotorsLettersIntoMappingNumbers(ROTOR_II)
+
+ROTOR_III   = "BDFHJLCPRTXVZNYEIWGAKMUSQO"
+ROTOR_III_NUMS = TurnRotorsLettersIntoMappingNumbers(ROTOR_III)
+
+print(ROTOR_I_NUMS)
+
+#ROTOR_IV    = ["E","S","O","V","P","Z","J","A","Y","Q","U","I","R","H","X","L","N","F","T","G","K","D","C","M","W","B"]
+#ROTOR_V     = ["V","Z","B","R","G","I","T","Y","U","P","S","D","N","H","L","X","A","W","M","J","Q","O","F","E","C","K"]
+#UKW_A       = ["E","J","M","Z","A","L","Y","X","V","B","W","F","C","R","Q","U","O","N","T","S","P","I","K","H","G","D"]	 	 	 
+UKW_B       = "YRUHQSLDPXNGOKMIEBFZCWVJAT"	 
+UKW_B_NUMS = TurnRotorsLettersIntoMappingNumbers(UKW_B)
+
+#UKW_C       = ["F","V","P","J","I","A","O","Y","E","D","R","Z","X","W","G","C","T","K","U","Q","S","B","N","M","H","L"]
+
 class Rotor():
     def __init__(self,newRotorType,newPos):
         if(newRotorType == "I"):
-            self.rotorList = ROTOR_I
+            self.rotorMapping = ROTOR_I_NUMS
             self.notch = "Y"
             self.turnover = "Q"
         elif(newRotorType == "II"):
-            self.rotorList = ROTOR_II
+            self.rotorMapping = ROTOR_II_NUMS
             self.notch = "M"
             self.turnover = "E"
         elif(newRotorType == "III"):
-            self.rotorList = ROTOR_III
+            self.rotorMapping = ROTOR_II_NUMS
             self.notch = "D"
             self.turnover = "V"
         
@@ -64,15 +84,20 @@ class Rotor():
             self.Rotate()
             somePos = somePos - 1
 
-    def GetLetter(self,someLetter):
-        pos = LetterToPos(someLetter)
-        return self.rotorList[pos]
-
-    def Rotate(self):
-        #Move once the rotor once
-        endLetter = self.rotorList.pop()
-        self.rotorList.insert(0,endLetter)
+    def GetMappedLetter(self,inputValue,forward=True):
+        if(forward):
+            return self.rotorMapping[inputValue]
+        else:
+            pos = self.rotorMapping.index(inputValue)
+            return pos
         
+    def Rotate(self):
+        #Move once the rotor forwards once - This puts the end letter onto the front of the list and shuffles things along
+        lastThing = self.rotorMapping.pop()
+        self.rotorMapping.insert(0,lastThing)
+        
+        #Should really return true or false to say if notch is moving the next wheel across!
+        #TODO
 
 class Enigma():
     def __init__(self,newRotor1,newRotor2,newRotor3):
@@ -81,23 +106,24 @@ class Enigma():
         self.rotor3 = newRotor3
 
     def scrambleMessage(self,incoming):
+
         cypher = ""
         for letter in incoming:
+            pos = LetterToPos(letter)
+            self.rotor1.Rotate()  #It rotates BEFORE the cypher happens!!!
             finalLetter = ""
-            r1Out = self.rotor1.GetLetter(letter)
-            finalLetter = r1Out
-            cypher = cypher + finalLetter
-            self.rotor1.Rotate()
+            r1Out = self.rotor1.GetMappedLetter(pos)
+            cypher = cypher + PosToLetter(r1Out)
 
             #TO DO - must pass through all rotors twice and reflector
             #ROTORS HAVE TO MOVE - NOTCHES DO THINGS, etc.
 
         return cypher
 
-inText = "zzz".upper()
+inText = "aaa".upper()
 print("In  : " + inText)
 
-r1 = Rotor("I",0)
+r1 = Rotor("I",25)
 r2 = Rotor("I",0)
 r3 = Rotor("I",0)
 
